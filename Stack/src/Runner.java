1c6307c1943a938ed3a1fb69b4c7505e1c1322f0
Bhavani <bhavanitechit@gmail.com>
2020-12-07 18:50:19 +0530

    Dynamic stack

--- Stack/src/Runner.java
+++ Stack/src/Runner.java
@@ -5,7 +5,22 @@ public class Runner {
 		
 		Stack stack = new Stack();
 		stack.push(5);
+		stack.push(15);
 		stack.show();
+		stack.push(25);
+		stack.push(35);
+		stack.show();
+		stack.push(45);
+		stack.show();
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		stack.show();
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		stack.show();
+		System.out.println(stack.pop());
+		stack.show();
+		System.out.println(stack.pop());
 	}
 
 }
--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -7,11 +7,76 @@ public class Stack {
 
 	public void push(int data)
 	{
-		stack[top] = data;
+		if(size() == capacity)
+			expand();
+		stack[top++] = data;
+	}
+	
+	public int pop()
+	{
+		int data = 0;
+		if(isEmpty())
+		{
+			System.out.println("Stack is Empty");
+		}
+		else
+		{
+			if(size() <= (capacity/2)/2)
+				shrink();
+			data = stack[--top];
+		}
+		return data;
+		
 	}
 	
 	public void show()
 	{
-		System.out.println(stack[top]);
+		if(isEmpty())
+			System.out.println("No data");
+		else
+		{
+			for(int i=size()-1; i>=0; i--)
+				System.out.print(stack[i]+ " ");
+			System.out.println();
+		}
+		
+		
+	}
+	
+	public boolean isEmpty()
+	{
+		return size() == 0;
+	}
+
+	
+	public void expand()
+	{
+		int[] newStack = new int[capacity*2];
+		int length = size();
+		System.arraycopy(stack, 0, newStack, 0, length);
+		capacity = capacity*2;
+		stack = newStack;
+	}
+	
+	
+	
+	public int peek()
+	{
+		return stack[top-1];
+	}
+	
+	public int size()
+	{
+		return top;
+	}
+	
+	public void shrink()
+	{
+		int[] newStack = new int[capacity/2];
+		int length = size();
+		System.arraycopy(stack, 0, newStack, 0, length);
+		capacity = capacity/2;
+		stack = newStack;
+		
 	}
 }
